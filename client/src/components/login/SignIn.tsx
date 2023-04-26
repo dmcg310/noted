4afdba0cf4b78fcdcca449eb925a4ee2a087d82a
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-04-26 16:37:42 +0100

    feat: sign in/out css

--- client/src/components/login/SignIn.tsx
+++ client/src/components/login/SignIn.tsx
@@ -20,8 +20,17 @@ const SignIn = () => {
 	const handleSignIn = async (e: React.FormEvent) => {
 		e.preventDefault();
 
+		document.getElementById("user-not-found")!.style.display = "none";
+		document.getElementById("invalid-password")!.style.display = "none";
+		document.getElementById("invalid-email")!.style.display = "none";
+		document.getElementById("error-signing-in")!.style.display = "none";
+
 		try {
-			const userCredential = await signInWithEmailAndPassword(auth, email, password);
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
 			const data = await signIn(userCredential.user.email!, password);
 			const userEmail = userCredential.user.email;
 
@@ -39,13 +48,15 @@ const SignIn = () => {
 		} catch (error) {
 			if (error instanceof FirebaseError) {
 				if (error.code === "auth/user-not-found") {
-					alert("User not found");
+					document.getElementById("user-not-found")!.style.display = "block";
 				} else if (error.code === "auth/wrong-password") {
-					alert("Invalid password");
+					document.getElementById("invalid-password")!.style.display = "block";
 				} else if (error.code === "auth/invalid-email") {
-					alert("Invalid email");
+					document.getElementById("invalid-email")!.style.display = "block";
+				} else if (password === "") {
+					document.getElementById("invalid-password")!.style.display = "block";
 				} else {
-					alert("Error signing in");
+					document.getElementById("error-signing-in")!.style.display = "block";
 				}
 			}
 		}
@@ -53,35 +64,107 @@ const SignIn = () => {
 
 	return (
 		<div>
-			<h1>Sign In</h1>
-			<form onSubmit={handleSignIn}>
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					name="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
+			<div
+				id="user-header"
+				className="w-screen flex justify-between p-2.5 pl-3 fixed bg-white"
+				style={{ alignItems: "center" }}
+			>
+				<button
+					className="px-4 py-2 text-3xl transition duration-100 ease-in rounded-md shadow-md text-slate-600 bg-slate-100 hover:bg-slate-200 text"
+					onClick={() => navigate("/")}
+				>
+					noted.
+				</button>
+			</div>
+			<div className="flex items-center justify-center h-screen bg-slate-200">
+				<div className="w-1/3 h-auto p-8 bg-white rounded-md shadow-lg">
+					<h1 className="pb-3 text-3xl text-slate-700">Sign in</h1>
+					<p
+						id="user-not-found"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						User not found!
+					</p>
+					<p
+						id="invalid-password"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Invalid password!
+					</p>
+					<p
+						id="invalid-email"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Invalid email!
+					</p>
+					<p
+						id="error-signing-in"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Error signing in!
+					</p>
+					<form onSubmit={handleSignIn}>
+						<div className="flex flex-col">
+							<label htmlFor="email" className="text-1xl text-slate-700">
+								Email
+							</label>
+							<input
+								type="text"
+								name="email"
+								id="email"
+								placeholder="email@gmail.com"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								className="p-2 mt-2 mb-4 border-2 rounded-md shadow-sm border-slate-200 focus:outline-none focus:border-blue-500"
+							/>
 
-				<label htmlFor="password">Password</label>
-				<input
-					type="password"
-					name="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-
-				<label htmlFor="remember-me">Remember me</label>
-				<input
-					type="checkbox"
-					name="remember-me"
-					id="remember-me"
-					checked={rememberMe}
-					onChange={(e) => setRememberMe(e.target.checked)}
-				/>
-
-				<button type="submit">Sign In</button>
-			</form>
+							<label htmlFor="password" className="text-1xl text-slate-700">
+								Password
+							</label>
+							<input
+								type="password"
+								name="password"
+								id="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="p-2 mt-2 mb-4 border-2 rounded-md shadow-sm border-slate-200 focus:outline-none focus:border-blue-500"
+							/>
+							<div className="flex items-center gap-2 pb-4">
+								<label
+									htmlFor="remember-me"
+									className="text-1xl text-slate-700"
+								>
+									Remember me
+								</label>
+								<input
+									type="checkbox"
+									name="remember-me"
+									id="remember-me"
+									checked={rememberMe}
+									onChange={(e) => setRememberMe(e.target.checked)}
+								/>
+							</div>
+						</div>
+						<button
+							type="submit"
+							className="w-full px-8 py-4 mb-4 text-2xl transition duration-100 ease-in rounded-md shadow-lg bg-emerald-500 hover:bg-emerald-400 text-slate-100"
+						>
+							Sign in!
+						</button>
+					</form>
+					<p className="text-1xl text-slate-700">
+						New? Sign up{" "}
+						<a href="/sign-up" className="text-blue-500 hover:text-blue-400">
+							here
+						</a>
+						.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
--- client/src/components/login/SingUp.tsx
+++ client/src/components/login/SingUp.tsx
@@ -14,6 +14,12 @@ const SignUp = () => {
 	const handleSignUp = async (e: React.FormEvent) => {
 		e.preventDefault();
 
+		document.getElementById("email-in-use")!.style.display = "none";
+		document.getElementById("invalid-email")!.style.display = "none";
+		document.getElementById("weak-password")!.style.display = "none";
+		document.getElementById("network-error")!.style.display = "none";
+		document.getElementById("account-exists")!.style.display = "none";
+
 		try {
 			const userCredential = await createUserWithEmailAndPassword(
 				auth,
@@ -26,21 +32,25 @@ const SignUp = () => {
 		} catch (error) {
 			if (error instanceof FirebaseError) {
 				if (error.code === "auth/email-already-in-use") {
-					alert("Email already in use, would you like to sign in?");
+					document.getElementById("email-in-use")!.style.display = "block";
 				} else if (error.code === "auth/invalid-email") {
-					alert("Invalid email");
+					document.getElementById("invalid-email")!.style.display = "block";
 				} else if (error.code === "auth/weak-password") {
-					alert("Password should be at least 6 characters");
+					document.getElementById("weak-password")!.style.display = "block";
 				} else if (error.code === "auth/network-request-failed") {
-					alert("Network error");
+					document.getElementById("network-error")!.style.display = "block";
 				} else if (
 					error.code === "auth/account-exists-with-different-credential"
 				) {
-					alert("Account already exists with different credentials");
+					document.getElementById("account-exists")!.style.display = "block";
 				} else if (error.code === "auth/credential-already-in-use") {
-					alert("Credential already in use");
+					alert("Credential already in use!");
+				} else if (password.length < 6) {
+					document.getElementById("weak-password")!.style.display = "block";
+				} else if (email === "") {
+					document.getElementById("invalid-email")!.style.display = "block";
 				} else {
-					alert("Error signing up");
+					document.getElementById("network-error")!.style.display = "block";
 				}
 			}
 		}
@@ -48,26 +58,112 @@ const SignUp = () => {
 
 	return (
 		<div>
-			<h1>Sign Up</h1>
-			<form onSubmit={handleSignUp}>
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					name="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-
-				<label htmlFor="password">Password</label>
-				<input
-					type="password"
-					name="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-
-				<button type="submit">Sign Up</button>
-			</form>
+			<div
+				id="user-header"
+				className="w-screen flex justify-between p-2.5 pl-3 fixed bg-white"
+				style={{ alignItems: "center" }}
+			>
+				<button
+					className="px-4 py-2 text-3xl transition duration-100 ease-in rounded-md shadow-md text-slate-600 bg-slate-100 hover:bg-slate-200 text"
+					onClick={() => navigate("/")}
+				>
+					noted.
+				</button>
+			</div>
+			<div className="flex items-center justify-center h-screen bg-slate-200">
+				<div className="w-1/3 h-auto p-8 bg-white rounded-md shadow-lg">
+					<h1 className="pb-3 text-3xl text-slate-700">Sign up</h1>
+					<p
+						id="email-in-use"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Email already in use!
+					</p>
+					<p
+						id="invalid-email"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Invalid email!
+					</p>
+					<p
+						id="weak-password"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Password should be at least 6 characters!
+					</p>
+					<p
+						id="network-error"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Network error!
+					</p>
+					<p
+						id="account-exists"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Account already exists with different credentials!
+					</p>
+					<p
+						id="credential-already-in-use"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Credential already in use!
+					</p>
+					<p
+						id="error-signing-up"
+						style={{ display: "none" }}
+						className="text-2xl text-red-500"
+					>
+						Error signing up!
+					</p>
+					<form onSubmit={handleSignUp}>
+						<div className="flex flex-col">
+							<label htmlFor="email" className="text-1xl text-slate-700">
+								Email
+							</label>
+							<input
+								type="text"
+								name="email"
+								id="email"
+								placeholder="email@gmail.com"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								className="p-2 mt-2 mb-4 border-2 rounded-md shadow-sm border-slate-200 focus:outline-none focus:border-blue-500"
+							/>
+							<label htmlFor="password" className="text-1xl text-slate-700">
+								Password
+							</label>
+							<input
+								type="password"
+								name="password"
+								id="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="p-2 mt-2 mb-4 border-2 rounded-md shadow-sm border-slate-200 focus:outline-none focus:border-blue-500"
+							/>
+						</div>
+						<button
+							type="submit"
+							className="w-full px-8 py-4 mb-4 text-2xl transition duration-100 ease-in rounded-md shadow-lg bg-emerald-500 hover:bg-emerald-400 text-slate-100"
+						>
+							Sign up!
+						</button>
+					</form>
+					<p className="text-1xl text-slate-700">
+						Already have an account? Sign in{" "}
+						<a href="/sign-in" className="text-blue-500 hover:text-blue-400">
+							here
+						</a>
+						.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
